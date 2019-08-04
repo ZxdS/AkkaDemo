50bf912ba5c90b3aa5ded3385e14efa5995ecc32
renyong <497269678@qq.com>
2019-08-04 18:26:13 +0800

    状态改变

--- src/main/java/com/luban/akka/vip/入门/BecomeDemo.java
+++ src/main/java/com/luban/akka/vip/入门/BecomeDemo.java
@@ -17,12 +17,27 @@ import akka.japi.function.Procedure;
 public class BecomeDemo {
 
     static class BecomeActor extends AbstractActor {
+        private Integer count = 0;
 
-        Receive receive = receiveBuilder()
+
+        Receive receiveC = receiveBuilder()
                 .matchAny(s -> {
-                    System.out.println("receive " + s);
                     if (s.equals("unbecome")) {
                         getContext().unbecome();
+                    } else {
+                        System.out.println("优惠100");
+                    }
+                })
+                .build();
+
+        Receive receiveB = receiveBuilder()
+                .matchAny(s -> {
+                    if (s.equals("unbecome")) {
+                        getContext().unbecome();
+                    } if (s.equals("become")) {
+                        getContext().become(receiveC, false);
+                    } else {
+                        System.out.println("优惠500");
                     }
                 })
                 .build();
@@ -31,9 +46,10 @@ public class BecomeDemo {
         public Receive createReceive() {
             return receiveBuilder()
                     .matchAny((s) -> {
-                        System.out.println("接收消息 " + s);
-                        if (s.equals("become")) {
-                            getContext().become(receive);
+                        count++;
+                        System.out.println("优惠1000");
+                        if (count == 3) {
+                            getContext().become(receiveB);
                         }
                     })
                     .build();
@@ -43,10 +59,15 @@ public class BecomeDemo {
     public static void main(String[] args) {
         ActorSystem system = ActorSystem.create("sys");
         ActorRef actorRef = system.actorOf(Props.create(BecomeActor.class), "becomeActor");
-        actorRef.tell("test", ActorRef.noSender());
+        actorRef.tell("1", ActorRef.noSender());
+        actorRef.tell("2", ActorRef.noSender());
+        actorRef.tell("3", ActorRef.noSender());
+        actorRef.tell("4", ActorRef.noSender());
+        actorRef.tell("5", ActorRef.noSender());
+//        actorRef.tell("unbecome", ActorRef.noSender());
         actorRef.tell("become", ActorRef.noSender());
-        actorRef.tell("test", ActorRef.noSender());
+        actorRef.tell("6", ActorRef.noSender());
         actorRef.tell("unbecome", ActorRef.noSender());
-        actorRef.tell("test", ActorRef.noSender());
+        actorRef.tell("7", ActorRef.noSender());
     }
 }
